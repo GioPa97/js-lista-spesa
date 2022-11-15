2d478497a286269a0b43e5c1c5633d666ae607ac
GioPa97 <giovannipagliara97@gmail.com>
2022-11-15 15:16:04 +0100

    Stampa con ciclo while

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 /*Data una lista della spesa già compilata nel vostro codice, stampare in console gli elementi della lista individualmente, prima con un ciclo for, poi con un ciclo while.
 Quando le stampe in console sono corrette, stampare gli elementi in un elenco puntato in HTML, utilizzando uno dei due cicli scritti al punto precedente.*/
 
-let ListaSpesa = ["uova", "latte", "biscotti", "caffè", "burro", "pane", "marmellata"];
+const ListaSpesa = ["uova", "latte", "biscotti", "caffè", "burro", "pane", "marmellata"];
 
 for (i = 0; i < ListaSpesa.length; i++) {
     console.log(ListaSpesa[i]);
 }
+
+i = 0;
+let DaComprare = "";
+while (ListaSpesa[i]) {
+  DaComprare += ListaSpesa[i] + " ";
+  i++;
+}
+
+console.log(DaComprare);
